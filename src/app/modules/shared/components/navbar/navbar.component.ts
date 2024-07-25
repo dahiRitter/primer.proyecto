7f232156472c9a5ed7389845dcf07e4c192335d5
dahiRitter <dahianaritter@gmail.com>
2024-07-25 09:45:06 -0300

    trtabajamos con directiva *ngif para ocultar y  mostrar botones de autentificacion

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/modules/autentificacion/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  logueado = true; 
+  deslogueado = false;
+
+  
+
+  constructor(
+    public servicioAuth: AuthService,
+    public servicioRutas: Router
+  ){}
+
+  iniciar (){
+    this.logueado = false;
+    this.deslogueado = true;
+  }
+
+  cerrarSesion (){
+    this.deslogueado = false;
+    //va a eliminar el "token"actual del usuario
+    //token: estado actual del usuario en el navegador para matener la sesion abierta
+    this.servicioAuth.cerrarSesion(); 
+
+    this.servicioRutas.navigate(["/"]); //redirigimos a la raiz de la pagigina
+    this.logueado = true;
+}
 
 }
